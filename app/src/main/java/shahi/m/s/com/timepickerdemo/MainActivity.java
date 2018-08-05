78a876f15e62cb120166fefda74535d9809a087b
Shahi <shahi5472@github.com>
2018-08-05 13:22:54 +0600

    Initial commit

---
+++ app/src/main/java/shahi/m/s/com/timepickerdemo/MainActivity.java
@@ -0,0 +1,39 @@
+package shahi.m.s.com.timepickerdemo;
+
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.show);
+        button = (Button) findViewById(R.id.click);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+        textView.setText("Hour :" + hour + "Minute :" + minute);
+    }
+}
---
+++ app/src/main/java/shahi/m/s/com/timepickerdemo/TimePickerFragment.java
@@ -0,0 +1,22 @@
+package shahi.m.s.com.timepickerdemo;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+
+import java.util.Calendar;
+
+public class TimePickerFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), (TimePickerDialog.OnTimeSetListener) getActivity(), hour, minute, DateFormat.is24HourFormat(getActivity()));
+    }
+}
